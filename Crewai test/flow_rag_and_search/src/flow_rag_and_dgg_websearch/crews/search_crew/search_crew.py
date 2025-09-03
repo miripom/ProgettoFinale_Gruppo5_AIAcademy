22b82a4c8d11d517aa631195226e59caf40f01de
miripom <37905291+miripom@users.noreply.github.com>
2025-09-03 17:30:06 +0200

    feat(DEEPEVAL): first set of deepeval

--- Crewai test/flow_rag_and_search/src/flow_rag_and_dgg_websearch/crews/search_crew/search_crew.py
+++ Crewai test/flow_rag_and_search/src/flow_rag_and_dgg_websearch/crews/search_crew/search_crew.py
@@ -1,6 +1,7 @@
-from crewai import Agent, Crew, Task, Process
+from crewai import Crew, Task, Process
 from crewai.project import CrewBase, agent, crew, task
 from crewai.agents.agent_builder.base_agent import BaseAgent
+from deepeval.integrations.crewai import instrument_crewai, Agent
 from typing import List
 
 from crewai_tools import SerperDevTool
@@ -12,22 +13,17 @@ class SearchCrew:
     agents: List[BaseAgent]
     tasks: List[Task]
 
+    instrument_crewai()
     @agent
     def search_agent(self) -> Agent:
         return Agent(
             config=self.agents_config["search_agent"],  # type: ignore
             tools=[SerperDevTool()]
         )
-
     @task
     def search_task(self) -> Task:
-        return Task(config=self.tasks_config["search_task"])  # type: ignore
+        return Task(config=self.tasks_config["search_task"])
 
     @crew
     def crew(self) -> Crew:
-        return Crew(
-            agents=self.agents,
-            tasks=self.tasks,
-            process=Process.sequential,
-            verbose=True,
-        )
+        return Crew(agents=self.agents, tasks=self.tasks)
